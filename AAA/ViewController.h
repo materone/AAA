59982fc2a875a023a05af26dc94958beb02cb247
Materone <materone@gmail.com>
2013-11-21 23:13:23 +0000

    Initial Commit

---
+++ AAA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AAA
+//
+//  Created by tony on 13-11-21.
+//  Copyright (c) 2013年 tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AAATests/AAATests.m
@@ -0,0 +1,34 @@
+//
+//  AAATests.m
+//  AAATests
+//
+//  Created by tony on 13-11-21.
+//  Copyright (c) 2013年 tony. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AAATests : XCTestCase
+
+@end
+
+@implementation AAATests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
